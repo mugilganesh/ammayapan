63b36091b0018d07086cf9d3a31f6f3fa92e136e
htarab <85612957+htarab@users.noreply.github.com>
2024-07-09 23:46:46 +0530

    Added Footer section, Updating of Home Page in progress, Yet to make changes to About Page

---
+++ test.js
@@ -0,0 +1,7 @@
+// script.js
+window.onload = function() {
+    const progressBar = document.getElementById('progress-bar');
+    setTimeout(() => {
+        progressBar.style.offset = 1400;
+    }, 100);
+};
