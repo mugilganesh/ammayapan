a4cb73b8ec547142ef17bdaecaa705852f507ed3
htarab <85612957+htarab@users.noreply.github.com>
2024-07-14 00:12:31 +0530

    Added cards for members in About Us page

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,5 +1,43 @@
 window.onload = function () {
     document.body.classList.add("remove-scrolling");
+
+    var swiper = new Swiper(".slide_members_content", {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        // slidesPerGroup: 3,
+        loop: true,
+        // centerSlide: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        // loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 70,
+            },
+            520: {
+                slidesPerView: 2,
+                spaceBetween: 60,
+            },
+            950: {
+                slidesPerView: 3,
+                spaceBetween: 45,
+            },
+            1600: {
+                slidesPerView: 4,
+                spaceBetween: 8,
+            },
+        },
+      });
 }
 
 setTimeout(()=>{
